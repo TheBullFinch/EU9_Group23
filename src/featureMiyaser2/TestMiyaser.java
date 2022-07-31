9f4bafe11e43b9539f7afcaf13255658256655e6
TheBullFinch <105283141+TheBullFinch@users.noreply.github.com>
2022-07-31 20:20:50 +0100

    finished my awesome job

---
+++ src/featureMiyaser2/TestMiyaser.java
@@ -0,0 +1,9 @@
+package featureMiyaser2;
+
+public class TestMiyaser {
+
+    public static void main(String[] args) {
+        System.out.println("I added some good features to our project");
+    }
+
+}
