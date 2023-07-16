4054c7d90c171f245143758d5b206859c236ec15
Serdar <105122485+Serdar5806@users.noreply.github.com>
2023-07-16 23:50:30 +0100

    Delete src/featureSerdar directory

--- src/featureSerdar/testSerdar.java
+++
@@ -1,8 +0,0 @@
-package featureSerdar;
-
-public class testSerdar {
-    public static void main(String[] args) {
-
-        System.out.println("I added some good features to our project");
-    }
-}
